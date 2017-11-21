652a1fd464843789b761a69d57b76e944af2a2a6
CYRIL VELLA <cyril.vella@yahoo.com>
2017-11-21 16:29:27 +0100

    modif findAll

--- src/core/product/routeproduct.js
+++ src/core/product/routeproduct.js
@@ -1,8 +1,15 @@
+const bdd = require('app/core/bdd/bdd.js');
+
 function routeproduct(app)
 {
 
 app.get('/product', function (req, res) {
-    res.send('GET request to the homepage');
+  if (err) throw err;
+  db.collection("ProductSchema").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(results);
+    bdd.close();
+  })
 });
 
 app.get('/product/:id', function (req, res) {
