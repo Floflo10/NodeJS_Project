87c9276ad2e3ff8abe92b4b3eed14369c53d1bf0
hebustrolle <ismael.hadj13@gmail.com>
2017-11-21 11:13:21 +0100

    creation fichier bdd sur develop

---
+++ bdd.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+function connection(){
+
+
+  mongoose.connect('mongodb://127.0.0.1/dbcommerce', {useMongoClient: true}, (err) =>{
+
+    if(err){
+      throw err;
+    }
+
+    const OrderSchema = new mongoose.Schema({
+      code: String,
+      date: Date,
+      total: Number,
+      Status: String
+    });
+
+    const OrderlineSchema = new mongoose.Schema({
+      product: Number,
+      order: Number,
+      quantity: Number
+    });
+
+
+    const ProductSchema = new mongoose.Schema({
+      name: String,
+      Price: Number
+    });
+
+
+
+
+    const OrderModel = mongoose.model('Order', OrderSchema);
+    const OrderlineModel = mongoose.model('Orderline', OrderlineSchema);
+    const ProductModel = mongoose.model('Product', ProductSchema);
+
+
+
+}
+
+
+exports.connection = connection;
