3d36c4a3a5c7b73b7347cbd0cd0a17d758cc3cfc
floflo199721 <florian.martines@ynov.com>
2017-11-21 12:04:05 +0100

    WebserverStart
    Lancement du serveur Web

---
+++ src/core/webserver/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+class Webserver {
+    constructor() {
+
+        this.express = null;
+        this.server = null;
+    }
+
+    start(port, callback) {
+        this.express = express();
+
+        this.server = this.express.listen(port, (err) => {
+            if (typeof callback === 'function') {
+                callback(err, this.express);
+            }
+        });
+    }
+
+    close(callback) {
+        if (this.server === null) {
+            callback(new Error('Web Server is not runnug'));
+        } else {
+            this.server.close(((err) => {
+                if (typeof callback === 'function') {
+                    callback(err);
+                }
+            }
+            ));
+        }
+    }
+}
+
+export default new Webserver();
