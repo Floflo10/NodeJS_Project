c03f4220d913b8f21b82a45eeaac15e74883d6b2
ismael HADJ <ismael HADJ>
2017-11-21 09:59:48 +0100

    premier commit

---
+++ start.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1/tasks'), {useMongoClient: true}, (err) =>{
+
+  if(err){
+    throw err;
+  }
+
+  const TaskSchema = new mongoose.Schema({
+    name: String,
+    done: Boolean
+  });
+
+  const TaskModel = mongoose.model('Task', TaskSchema);
+
+
+  let params = {
+    name : 'Ma tache'
+  }
+
+  TaskModel.create(params, (error, tasks) =>{
+    if(!error){
+      console.log(task);
+      TaskModel.find((error, tasks) => {
+        if(!error){
+          console.log(tasks);
+        }
+      });
+    }else{
+      console.log(error);
+    }
+  });
+
+}
