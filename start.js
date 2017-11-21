06246d18a1313a322e894b59caa19cfb0b909279
ismael HADJ <ismael HADJ>
2017-11-21 10:40:35 +0100

    bdd et package json

--- start.js
+++ start.js
@@ -1,28 +1,47 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1/tasks', {useMongoClient: true}, (err) =>{
+mongoose.connect('mongodb://127.0.0.1/dbcommerce', {useMongoClient: true}, (err) =>{
 
   if(err){
     throw err;
   }
 
-  const TaskSchema = new mongoose.Schema({
-    name: String,
-    done: Boolean
+  const OrderSchema = new mongoose.Schema({
+    code: String,
+    date: Date,
+    total: Number,
+    Status: String
   });
 
-  const TaskModel = mongoose.model('Task', TaskSchema);
+  const OrderlineSchema = new mongoose.Schema({
+    product: Number,
+    order: Number,
+    quantity: Number
+  });
+
+
+  const ProductSchema = new mongoose.Schema({
+    name: String,
+    Price: Number
+  });
+
+
+
+
+  const OrderModel = mongoose.model('Order', OrderSchema);
+  const OrderlineModel = mongoose.model('Orderline', OrderlineSchema);
+  const ProductModel = mongoose.model('Product', ProductSchema);
 
 
   let params = {
-    name : 'Ma tache'
-  }
+    Status : 'Mon ordre'
+  };
 
-  TaskModel.create(params, (error, tasks) =>{
+  OrderModel.create(params, (error, tasks) =>{
     if(!error){
       console.log(tasks);
-      TaskModel.find((error, tasks) => {
+      OrderModel.find((error, tasks) => {
         if(!error){
           console.log(tasks);
         }
