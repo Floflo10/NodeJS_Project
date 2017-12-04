782c3326c08e000ddd0992279612e0c4406f6296
floflo199721 <florian.martines@ynov.com>
2017-12-04 11:46:00 +0100

    line en cours

--- src/core/line/routeline.js
+++ src/core/line/routeline.js
@@ -1,9 +1,13 @@
+
 function routeline(app)
 {
 
 
 app.put('/order/:orderid/line/', function (req, res) {
     var orderid = req.params.orderid;
+
+    addline(req.body.product, req.body.order, req.body.quantity);
+
 });
 
 app.delete('/order/:orderid/line/:lineid', function (req, res) {
