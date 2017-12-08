6a454e022eea821a46bd909b69d68766c29c087d
hebustrolle <ismael.hadj13@gmail.com>
2017-12-08 23:14:29 +0100

    test

--- test/core/webserver/index.js
+++ test/core/webserver/index.js
@@ -1,53 +1,15 @@
-const express = require('express');
-const bdd = require('app/core/bdd');
-const route = require('app/core/routage');
-const bodyParser = require('body-parser');
+import { expect } from 'chai';
+import Webserver from 'app/core/webserver';
 
-/*
-*
-* @author VELLA CYRIL <cyril.vella@ynov.com>
-* @copyright VELLA CYRIL - 2017
-*/
+describe('Testing webserver', () => {
+    it('is a bad test example', () => {
+        expect(false).to.be.false;
+    });
 
-/**
- * Création du serveur web
- * @class
- */
-
-class Webserver {
-
-    constructor() {
-        this.express = null;
-        this.server = null;
-
-    }
-
-    start(port, callback) {
-        this.express = express();
-
-        this.express.use(bodyParser.json());
-        this.express.use(bodyParser.urlencoded({ extended: true }));
-
-        this.server = this.express.listen(port, (err) => {
-            if (typeof callback === 'function') {
-                callback(err, this.express);
-            }
-
-        });
-    }
-
-    close(callback) {
-        if (this.server === null) {
-            callback(new Error('Web Server is not running'));
-        } else {
-            this.server.close(((err) => {
-                if (typeof callback === 'function') {
-                    callback(err);
-                }
-            }
-            ));
-        }
-    }
-}
-
-export default new Webserver();
+    it('sould run correctly', () => {
+        Webserver.start(3001, (err) => {
+            expect(err).to.not.exist;
+            done();
+        })
+    })
+});
